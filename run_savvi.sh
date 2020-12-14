4c0b0bd9ae8a847e18c82cb882f6bf53e6751bbb
ryooit <ryooit70@gmail.com>
2020-12-14 14:02:44 +0900

    Reduce batch size per gpu'

--- run_savvi.sh
+++ run_savvi.sh
@@ -4,6 +4,7 @@ python examples/language-modeling/run_clm.py \
     --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
+    --per-gpu-train-batch-size 4 \
     --do_train \
     --do_eval \
     --output_dir /tmp/test-clm
