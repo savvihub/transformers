19292d1acb7b5b0f7dcc1c53ec41d2eb75077ddc
ryooit <ryooit70@gmail.com>
2020-12-14 14:06:45 +0900

    typo

--- run_savvi.sh
+++ run_savvi.sh
@@ -4,7 +4,7 @@ python examples/language-modeling/run_clm.py \
     --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per-gpu-train-batch-size 4 \
+    --per_device_train_batch_size 4 \
     --do_train \
     --do_eval \
     --output_dir /tmp/test-clm
