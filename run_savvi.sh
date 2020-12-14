c77c6e9ff7094b729375c16fed081432516ecba2
ryooit <ryooit70@gmail.com>
2020-12-14 12:10:51 +0900

    typo

--- run_savvi.sh
+++ run_savvi.sh
@@ -1,6 +1,6 @@
 pip install .
 pip install -r examples/language-modeling/requirements.txt
-python run_clm.py \
+python examples/language-modeling/run_clm.py \
     --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
