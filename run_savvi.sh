53ce517b0d1f3e2c710193542ac1c50b5697fc8f
ryooit <ryooit70@gmail.com>
2020-12-14 14:17:25 +0900

    Try batch size 2

--- run_savvi.sh
+++ run_savvi.sh
@@ -4,7 +4,8 @@ python examples/language-modeling/run_clm.py \
     --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 2 \
+    --output_dir '/output' \
     --do_train \
     --do_eval \
     --output_dir /tmp/test-clm
